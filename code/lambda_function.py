7f0a7fc2052cb239e5252d16f52dcc2a8f78fbc2
pratheekshavrao <156860208+pratheekshavrao@users.noreply.github.com>
2024-01-18 18:38:56 -0700

    Add files via upload

---
+++ code/lambda_function.py
@@ -0,0 +1,14 @@
+import boto3
+
+def lambda_handler(event, context):
+    ec2_resource = boto3.resource('ec2')
+
+    for elastic_ip in ec2_resource.vpc_addresses.all():
+        if elastic_ip.instance_id is None:
+            elastic_ip.release()
+            print(f"{elastic_ip} not associated with any instance.Hence releasing.")
+    
+    return {
+        'statusCode': 200,
+        'body': 'Processed EIP\'s!'
+    }
